22a456ea1099fceb3f36150932bdf5b61fff64fe
shalindileesha <shalindileesha@gmail.com>
2024-06-24 16:36:08 +0530

    Initial commit

---
+++ src/main/java/com/ecom/EcomWebApp/entity/User.java
@@ -0,0 +1,24 @@
+package com.ecom.EcomWebApp.entity;
+
+import com.ecom.EcomWebApp.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+    private String password;
+    private String name;
+    private UserRole userRole;
+
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] img;
+}
