4e90d92acf4c17b4475ae040ea709febcb50fd48
shalindileesha <shalindileesha@gmail.com>
2024-08-01 12:24:21 +0530

    created search bar

--- src/main/java/com/ecom/EcomWebApp/config/WebSecurityConfiguration.java
+++ src/main/java/com/ecom/EcomWebApp/config/WebSecurityConfiguration.java
@@ -27,7 +27,7 @@ public class WebSecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/authenticate","/sign-up","/order/**","api/admin/category","api/admin","api/admin/product","api/admin/products")
+                .requestMatchers("/authenticate","/sign-up","/order/**","api/admin/category","api/admin","api/admin/product","api/admin/products","api/admin/search/*")
                 .permitAll()
                 .and()
                 .authorizeHttpRequests()
